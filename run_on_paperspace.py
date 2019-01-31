24423737a8a0886d530dc838bf4799bc4ca68c30
Mitchell, Mark <mark.mitchell@fjordnet.com>
2019-01-31 21:41:12 +1100

    put output into artifacts

--- run_on_paperspace.py
+++ run_on_paperspace.py
@@ -10,4 +10,12 @@ mkdir -p /storage/${MY_DRIVE}/datasets/fashion-mnist/
 mkdir -p /storage/${MY_DRIVE}/results/gan/
 
 # train
-python main.py --gan_type BEGAN --dataset fashion-mnist --epoch 50 --batch_size 64 --result_dir /storage/${MY_DRIVE}/results/gan/
+python main.py --gan_type BEGAN --dataset fashion-mnist --epoch 50 --batch_size 64 --result_dir /storage/results/gan/
+
+echo "Listing out /storage/results/gan"
+ls /storage/results/gan
+echo "-------------------"
+echo "Moving /storage/results/gan to artifacts"
+mv /storage/results/ /artifacts/
+echo "Listing /artifacts:"
+ls /artifacts
